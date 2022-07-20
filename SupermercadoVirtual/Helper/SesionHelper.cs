f04a3a07241e81c4b3e632fb2e331207dd4403cb
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-07-20 10:33:16 -0400

    Creando el helper para convertir objeto->json

---
+++ SupermercadoVirtual/Helper/SesionHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+namespace SupermercadoVirtual.Helper
+{
+    public static class SesionHelper
+    {
+        public static void SetObjetoToJson(this ISession sesion, string clave, object valor) {
+            sesion.SetString(clave, JsonConvert.SerializeObject(valor));
+        }
+        public static T GetJsonToObjeto<T>(this ISession sesion, string clave) {
+            return sesion.GetString(clave) == null ? default(T) : JsonConvert.DeserializeObject<T>(sesion.GetString(clave));
+        }
+    }
+}
