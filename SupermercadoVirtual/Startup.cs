abff07ba12bb99c2cf4c22bc2de9bb214cacd488
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-07-20 10:21:05 -0400

    Agegando servico de sesión.

--- SupermercadoVirtual/Startup.cs
+++ SupermercadoVirtual/Startup.cs
@@ -24,6 +24,7 @@ namespace SupermercadoVirtual
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,7 @@ namespace SupermercadoVirtual
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
